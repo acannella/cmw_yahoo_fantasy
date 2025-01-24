75ca4ed145f638a50f1f4a39c651d7844236b1e7
Andrew Cannella <cannella0andrew@gmail.com>
2025-01-24 09:54:15 -0800

    Create nflPlayersController.js

---
+++ src/controllers/nflPlayersController.js
@@ -0,0 +1,10 @@
+const { PrismaClient } = require('@prisma/client');
+const YahooFantasy = require('yahoo-fantasy');
+
+const refreshAccessToken = require('../../utils/refreshAccessToken.js');
+
+const prisma = new PrismaClient();
+
+const getAllFantasyRosters = async function () {};
+
+const updateFantasyRosters = async function () {};
