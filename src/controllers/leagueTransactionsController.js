e51b7e714009fee75c2978e1602eb57b53578417
Andrew Cannella <cannella0andrew@gmail.com>
2025-01-27 01:37:30 -0800

    WIP: add test Date objects to verify I can compare dates taken from the DB

--- src/controllers/leagueTransactionsController.js
+++ src/controllers/leagueTransactionsController.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const initYahooFantasy = require('../../utils/initYahooFantasy');
 const getLeagueMetadata = require('../../utils/getLeagueMetadata');
+const dateWithTimeZoneOffset = require('../../utils/dateWithTimeZoneOffset');
 
 const prisma = new PrismaClient();
 
@@ -10,9 +11,15 @@ const getTransactionsForWeek = async function (week) {
     where: { league_key: leagueMetadata.league_key, week_number: week },
     select: { week_number: true, week_end: true, week_start: true },
   });
-  const testDate = new Date(gameWeek.week_end);
-  testDate.setDate(testDate.getDate() + 5);
+  const testDate = new Date(gameWeek.week_end).toLocaleString();
+  const ddDate = new Date('2024-09-25 08:05:48.000').toLocaleString();
+  const dbDate = dateWithTimeZoneOffset(
+    '2024-09-25 08:05:48.000'
+  ).toLocaleString();
+
   console.log(testDate);
+  console.log(dbDate);
+  console.log(dbDate <= testDate);
 };
 
 getTransactionsForWeek(4);
