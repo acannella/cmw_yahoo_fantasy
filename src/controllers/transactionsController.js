243bd22baa924ad74b7954aae0ae9de19bfbb72d
Andrew Cannella <cannella0andrew@gmail.com>
2025-01-29 00:52:55 -0800

    Remove console.log

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -2,7 +2,6 @@ const transactionsQueries = require('../queries/transactionsQueries');
 
 exports.transactionsForWeek = async function (req, res) {
   const week = +req.query.week;
-  console.log(week);
   const transactions = await transactionsQueries.getTransactionsForWeek(week);
   res.status(200).send(transactions);
 };
