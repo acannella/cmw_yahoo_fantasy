ae20d571005aaf502a3d434054cf2fc2938c97f6
Andrew Cannella <cannella0andrew@gmail.com>
2025-02-04 17:18:11 -0800

    Update controller to use model.js for getting the site data, remove logic in controller that was fetching data

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,47 +2,31 @@ import homePageView from '../views/homePageView.js';
 import rostersPageView from '../views/rostersPageView.js';
 import aboutPageView from '../views/aboutPageView.js';
 import recordBookPageView from '../views/recordBookView.js';
-
-const API_URL = 'http://localhost:3000/api/v1';
-let rosterData;
-let rosterDataLoaded = false;
+import * as model from './model.js';
 
 const controlTopScoringPlayersData = async function (week) {
-  const topScoringCall = await fetch(
-    `${API_URL}/topscoringplayers?year=2024&weekStart=${week}&weekEnd=${week}`
-  );
-
-  const topScoringPlayersData = await topScoringCall.json();
+  const topScoringPlayersData = await model.loadTopScoringPlayers(week);
   homePageView.renderTopScoringTable(topScoringPlayersData);
 };
 
 const controlTransactionsData = async function (week) {
-  const transactionsCall = await fetch(`${API_URL}/transactions?week=${week}`);
-
-  const transactionsData = await transactionsCall.json();
+  const transactionsData = await model.loadTransactionsByWeek(week);
   homePageView.renderTransactionsTable(transactionsData);
 };
 
 const controlStandingsData = async function () {
-  const standingsCall = await fetch(`${API_URL}/standings`);
-  const standingsData = await standingsCall.json();
+  const standingsData = await model.loadStandingsData();
   homePageView.renderStandingsTable(standingsData);
 };
 const controlRecordBookData = async function () {
-  const recordBookCall = await fetch(`${API_URL}/recordbook`);
-  const recordBookData = await recordBookCall.json();
-  recordBookPageView.displayRecords(recordBookData);
+  await model.loadRecordBookData();
 };
 
-//If this rosterData hasn't been stored yet, get the data, store the data, dispatch event
+//Dispatch event after roster data is loaded, this will remove the loading icon when navigating to the Rosters page
 const controlRostersData = async function () {
-  if (!rosterData) {
-    const rostersCall = await fetch(`${API_URL}/rosters`);
-    rosterData = await rostersCall.json();
-    rosterDataLoaded = true;
-    const event = new Event('rosters_loaded');
-    window.dispatchEvent(event);
-  }
+  await model.loadRosterData();
+  const rostersLoadedEvent = new Event('rosters_loaded');
+  window.dispatchEvent(rostersLoadedEvent);
 };
 
 const controlNavigation = function (buttonID) {
@@ -50,16 +34,16 @@ const controlNavigation = function (buttonID) {
     homePageView.rebuildHomePage();
   }
   if (buttonID.includes('rosters')) {
-    rostersPageView.displayLoadingIcon();
-    if (rosterDataLoaded) {
+    if (!model.state.rosterData) rostersPageView.displayLoadingIcon();
+    if (model.state.rosterData) {
       rostersPageView.clearHTML();
-      rostersPageView.displayRosters(rosterData);
+      rostersPageView.displayRosters(model.state.rosterData);
     }
   }
   if (buttonID.includes('newsletters')) {
   }
   if (buttonID.includes('record')) {
-    controlRecordBookData();
+    recordBookPageView.displayRecords(model.state.recordBookData);
   }
   if (buttonID.includes('about')) {
     aboutPageView.renderAboutPage();
@@ -67,6 +51,7 @@ const controlNavigation = function (buttonID) {
 };
 
 const init = function () {
+  controlRecordBookData();
   controlRostersData();
   homePageView.initHomePage([
     controlTopScoringPlayersData,
