2fc5aed9d04458b72e34bd69f175023376426e54
Andrew Cannella <cannella0andrew@gmail.com>
2025-02-06 20:17:51 -0800

    Add generateContainers and clearDataContainers functions

--- public/views/newsletterPageView.js
+++ public/views/newsletterPageView.js
@@ -5,17 +5,25 @@ class NewsletterPageView extends View {
     super();
   }
 
+  #generateContainers() {
+    this.header.insertAdjacentHTML('afterend', super.generateContainers());
+    const pageContainer = document.querySelector('.page-container');
+    pageContainer.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="data-container"></div>`
+    );
+  }
+
   renderPage(newslettersCount, currNewsletter, handler) {
+    this.#generateContainers();
     const dropdown = this.#generateDropdown(newslettersCount);
     const newsletterFrame = this.#generateNewsletterFrame(currNewsletter);
     const newsletterHeader = this.#generateheader(newslettersCount);
-    this.header.insertAdjacentHTML('afterend', this.generateContainers());
     const pageContainer = document.querySelector('.page-container');
+    pageContainer.insertAdjacentHTML('afterbegin', newsletterHeader);
     pageContainer
       .querySelector('.data-container')
-      .insertAdjacentHTML('beforeend', newsletterHeader + newsletterFrame);
-    pageContainer.insertAdjacentHTML('beforeend', dropdown);
-
+      .insertAdjacentHTML('afterbegin', newsletterFrame + dropdown);
     const weekDropdown = document.querySelector('.newsletter-week-dropdown');
     weekDropdown.value = newslettersCount;
     weekDropdown.addEventListener('change', function () {
@@ -30,7 +38,7 @@ class NewsletterPageView extends View {
 
       i++;
     }
-    return `<div class="newsletter-dropdown-container"><h3 class="newsletter-week-header">Week</h3><select class="newsletter-week-dropdown">${optionsString}</select></div>`;
+    return `<div class="dropdown-container"><h3 class="newsletter-week-header">Week</h3><select class="newsletter-week-dropdown">${optionsString}</select></div>`;
   }
   #generateNewsletterFrame(newsletter) {
     return `<iframe src="${newsletter}" width="800" height="750" marginwidth="0"
@@ -40,16 +48,21 @@ class NewsletterPageView extends View {
   #generateheader(number) {
     return `<div class="newsletter-header"><h2>Week ${number} Newsletter</h2></div>`;
   }
+  #clearDataContainer() {
+    super.clearDataContainer();
+    document.querySelector('.newsletter-header').remove();
+  }
+
   renderNewsletter(weekNumber, newsletter) {
-    const newsletterContainer = document.querySelector('.data-container');
+    const pageContainer = document.querySelector('.page-container');
     const newsletterHeader = this.#generateheader(weekNumber);
     const newsletterFrame = this.#generateNewsletterFrame(newsletter);
 
-    this.clearDataContainer();
-    newsletterContainer.insertAdjacentHTML(
-      'beforeend',
-      `${newsletterHeader}${newsletterFrame}`
-    );
+    this.#clearDataContainer();
+    pageContainer.insertAdjacentHTML('afterbegin', newsletterHeader);
+    pageContainer
+      .querySelector('.data-container')
+      .insertAdjacentHTML('afterbegin', newsletterFrame);
   }
 }
 
