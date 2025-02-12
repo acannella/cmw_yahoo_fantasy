a8699e175d930123e54ce76dac61648221807400
Andrew Cannella <cannella0andrew@gmail.com>
2025-02-11 17:51:24 -0800

    Update parameter names to be more descriptive

--- public/views/newsletterPageView.js
+++ public/views/newsletterPageView.js
@@ -22,10 +22,10 @@ class NewsletterPageView extends View {
    * Render newsletters page with the newsletter from the most recent week
    * @param {number} newslettersCount Number of newsletters in the database
    * @param {String} currNewsletter String of the URL to the newsletter in Google Docs
-   * @param {function} handler function called whenever the newsletter dropdown is changed
+   * @param {function} newsletterHandler function called whenever the newsletter dropdown is changed
    */
 
-  renderPage(newslettersCount, currNewsletter, handler) {
+  renderPage(newslettersCount, currNewsletter, newsletterHandler) {
     this.#generateContainers();
     const dropdown = this.#generateDropdown(newslettersCount);
     const newsletterFrame = this.#generateNewsletterFrame(currNewsletter);
@@ -38,20 +38,20 @@ class NewsletterPageView extends View {
     const weekDropdown = document.querySelector('.week-dropdown');
     weekDropdown.value = newslettersCount;
     weekDropdown.addEventListener('change', function () {
-      handler(this.value);
+      newsletterHandler(this.value);
     });
   }
 
   /**
    * Generate HTML String of dropdown container, dropdown, header, and dropdown options
-   * @param {number} number number of newsletters, used to populate the number of dropdown options
+   * @param {number} newsletterCount number of newsletters, used to populate the number of dropdown options
    * @returns {String} HTML string of the dropdown container
    */
 
-  #generateDropdown(number) {
+  #generateDropdown(newsletterCount) {
     let i = 1;
     let optionsString = '';
-    while (i <= number) {
+    while (i <= newsletterCount) {
       optionsString += `<option value="${i}">${i}</option>`;
 
       i++;
@@ -72,12 +72,12 @@ class NewsletterPageView extends View {
 
   /**
    * Generate HTMl String for the header
-   * @param {number} number Number that will be in the header
+   * @param {number} weekNumber Number that will be in the header
    * @returns HTML String with the newsletter-header data
    */
 
-  #generateheader(number) {
-    return `<div class="newsletter-header"><h2>Week ${number} Newsletter</h2></div>`;
+  #generateheader(weekNumber) {
+    return `<div class="newsletter-header"><h2>Week ${weekNumber} Newsletter</h2></div>`;
   }
 
   /**
