33a9e73d8a6ba97ad6b9799f705d8409c7511abf
Andrew Cannella <cannella0andrew@gmail.com>
2025-02-18 20:05:17 -0800

    Update variable from var to const, remove reference to public directory since that is now in the frontend, update the log message when the node app is run

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const fantasyRouter = require('./routes/fantasyRouter');
-var cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 app.use('/', fantasyRouter);
-app.use(express.static(__dirname + '/public'));
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(
+    `California's Most Wanted Fantasy Site listening on port ${port}`
+  );
 });
